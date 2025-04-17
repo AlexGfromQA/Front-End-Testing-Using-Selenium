aedb3aa07c303d960151a822cd7004e8b02fa1d3
AlexGFromQA <alexgriffiths83@googlemail.com>
2025-04-17 22:27:50 +0100

    Created 3 simple tests that use selenium to test URL navigation

---
+++ Frontend-Regression/Run/UnitTest1.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V133.FedCm;
+using System.Security.Cryptography;
+//using OpenQA.Selenium.Chrome.ChromeDriver;
+
+
+namespace Frontend_Regression.Run
+{
+    public class Tests
+    {
+
+        public void Setup()
+        {
+            //  
+        }
+
+        [Test]
+        public void Test_userCanAccessAboutPage()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://mansaalexgriffiths.wixsite.com/alexthescribe/");
+            driver.Manage().Window.Maximize();
+            // find about Page 
+            var button = driver.FindElement(By.Id("comp-iszt89yr0label"));
+            button.Click();
+            Assert.That(driver.Url.Equals("https://mansaalexgriffiths.wixsite.com/alexthescribe/about"));
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test_userCanAccessSubjectPages()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://mansaalexgriffiths.wixsite.com/alexthescribe/");
+            driver.Manage().Window.Maximize();
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test_userCanAccessBlog()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://mansaalexgriffiths.wixsite.com/alexthescribe/blog");
+            driver.Manage().Window.Maximize();
+            Console.WriteLine("run executed");
+            driver.Quit();
+
+            //verify title is shown? 
+        }
+
+
+
+    }
+}
